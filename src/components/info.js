6cc85f1e4e5fe0ab25bf5890bbe77cd494ebb172
JaredBartynski <70081376+JaredBartynski@users.noreply.github.com>
2020-11-09 10:56:15 -0500

    added html layout

---
+++ src/components/info.js
@@ -0,0 +1,40 @@
+<section class="container">
+  <section class="card mb-3" style="max-width: 540px">
+    <section class="row no-gutters">
+      <section class="col-md-4">
+        <img
+          src="images/58608497_10218716357490917_1622117445852463104_o.jpg"
+          class="card-img"
+          alt="..."
+        />
+      </section>
+      <section class="col-md-8">
+        <section class="card-body">
+          <h5 class="card-title">About Me</h5>
+          <ul>
+            <li>E-mail: jared117@live.com</li>
+            <li>
+              LinkedIn:https://www.linkedin.com/in/jared-bartynski-103a551b2/
+            </li>
+            <li>Github:https://github.com/JaredBartynski</li>
+            <li>Resume:https://my.indeed.com/p/jaredb-8z8cwbn</li>
+          </ul>
+          <br />
+          <p class="card-text">
+            I was born and raised in Allentown, Pennsylvania. I graduated from
+            Parkland High School and spent the next ten years working warehouse
+            jobs. I enjoyed the physical work but I found that it was not
+            mentally stimulating or challenging. I started to learn to code on
+            free websites and found that I really enjoyed it. This made me want
+            to pursue coding and thus I found the Penn boot camp that I am
+            currently attending. Once I graduate in November I will be a full
+            stack developer and will be excited to start my new career.
+          </p>
+          <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </section>
+      </section>
+    </section>
+  </section>
+</section>;
---
+++ src/components/navbar.js
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <h4>Jared Bartynski</h4>
+  <button
+    class="navbar-toggler"
+    type="button"
+    data-toggle="collapse"
+    data-target="#navbarText"
+    aria-controls="navbarText"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <section class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          About
+        </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="portfolio.html">
+          Portfolio <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.html">
+          Contact
+        </a>
+      </li>
+    </ul>
+  </section>
+</nav>;
