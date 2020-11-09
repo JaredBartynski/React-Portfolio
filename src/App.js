0dc18c1f782be3b62d57746cdbe14f61e60612d0
JaredBartynski <70081376+JaredBartynski@users.noreply.github.com>
2020-11-09 12:53:18 -0500

    components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
-import HelloReact from "./components/HelloReact";
+import info from "./components/info";
+import navbar from "./components/navbar";
+import contact from "./components/contact";
+import portfolio from "./components/portfolio";
 
 function App() {
-  return <HelloReact />;
+  return (
+    <>
+      <Router>
+        <navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,37 +1,48 @@
-<Section class="container">
-  <Section class="card" style="width: 18rem">
-    <Section class="card-body">
-      <h5 class="card-title">Contact</h5>
-      <form>
-        <Section class="form-group">
-          <label for="formGroupExampleInput">Name</label>
-          <input
-            type="text"
-            class="form-control"
-            id="formGroupExampleInput"
-            placeholder="Name"
-          />
+import React from "react";
+
+function contact() {
+  return (
+    <>
+      <Section class="container">
+        <Section class="card" style="width: 18rem">
+          <Section class="card-body">
+            <h5 class="card-title">Contact</h5>
+            <form>
+              <Section class="form-group">
+                <label for="formGroupExampleInput">Name</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="formGroupExampleInput"
+                  placeholder="Name"
+                />
+              </Section>
+              <Section class="form-group">
+                <label for="formGroupExampleInput2">Email</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="formGroupExampleInput2"
+                  placeholder="Email"
+                />
+              </Section>
+              <Section class="form-group">
+                <label for="formGroupExampleInput2">Message</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="formGroupExampleInput2"
+                  placeholder="Message"
+                />
+              </Section>
+            </form>
+            <input class="btn btn-primary" type="submit" value="Submit" />
+          </Section>
         </Section>
-        <Section class="form-group">
-          <label for="formGroupExampleInput2">Email</label>
-          <input
-            type="text"
-            class="form-control"
-            id="formGroupExampleInput2"
-            placeholder="Email"
-          />
-        </Section>
-        <Section class="form-group">
-          <label for="formGroupExampleInput2">Message</label>
-          <input
-            type="text"
-            class="form-control"
-            id="formGroupExampleInput2"
-            placeholder="Message"
-          />
-        </Section>
-      </form>
-      <input class="btn btn-primary" type="submit" value="Submit" />
-    </Section>
-  </Section>
-</Section>;
+      </Section>
+      ;
+    </>
+  );
+}
+
+export default contact;
